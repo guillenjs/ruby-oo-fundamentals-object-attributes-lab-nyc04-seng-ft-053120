cffb80be893d38907fd1731e4481c0cf98f5c717
guillenjs <jorgeguilln@gmail.com>
2020-06-23 14:59:33 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+    # def initialize(dog_name)
+    #     @name = (dog_name)
+    # end
+
+    def name=(dog_name)
+        @name = dog_name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(new_breed)
+        @breed = new_breed
+    end
+
+    def breed
+        @breed
+    end
+    
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+    def name=(new)
+        @name = new
+    end
+
+    def name
+        @name
+    end
+
+    def job=(position)
+        @job = position
+    end
+
+    def job 
+        @job
+    end
+    
+end
